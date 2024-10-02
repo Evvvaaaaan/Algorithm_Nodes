671709dd648c86ce668f2b6eaf9e32979951d395
Evvvaaaaan <110900656+Evvvaaaaan@users.noreply.github.com>
2024-10-02 22:07:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.py
@@ -0,0 +1,17 @@
+'''def MenOfPassion(A[], n) {
+  sum <- 0;
+  for i <- 1 to n
+      for j <- 1 to n
+          for k <- 1 to n
+              sum <- sum + A[i] × A[j] × A[k]; # 코드1
+  return sum;
+}
+
+i = 1 부터 n까지
+j = 1 부터 n 가지
+k = 1 부터 n RKwl 
+
+'''
+
+print(int(input())**3)
+print(3)
