5e7941336a80a347972b3070cfb25c9e088821ea
Evvvaaaaan <110900656+Evvvaaaaan@users.noreply.github.com>
2026-01-31 14:29:59 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,23 @@
+import sys
+input  = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+
+s = []
+
+
+def dfs(start):
+    if len(s) == M:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(start, N + 1):
+        s.append(i)
+
+        dfs(i+1)
+
+        s.pop()
+
+
+dfs(1)
